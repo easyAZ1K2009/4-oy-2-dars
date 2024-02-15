ce849f54420d6af2f45c565ed2247ee3000dfeee
easyAZ1K2009 <you@example.com>
2024-02-15 21:57:21 +0500

    xammayo tayyor

---
+++ app.js
@@ -0,0 +1,39 @@
+const yosh = "14";
+console.log(yosh);
+console.log(Number(yosh));
+
+console.log(yosh + 1);
+console.log(Number(yosh) + 1);
+
+const ism = "Muxammadaziz";
+console.log(Number(ism));
+
+const yil = "2009";
+console.log(yil);
+console.log(string(yil));
+
+console.log("Men" + 2009 + "tilda tavallut topganman");
+
+console.log("30" - "10" - 5);
+
+console.log(Boolean(false));
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean(0n));
+
+console.log(Boolean("Muxammadaziz"));
+console.log(Boolean(23));
+console.log(Boolean([]));
+console.log(Boolean({}));
+console.log(Boolean("0"));
+console.log(Boolean("false"));
+
+const Ism = "";
+if (Ism) {
+  console.log("Sizning ismingiz" + Ism);
+} else {
+  console.log("Iltimos ism kiriting");
+}
